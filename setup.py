e492ab63bb74250e061d1235813397a88cef9747
kalloc <yourbunnywrote@ya.ru>
2013-01-30 16:42:14 +0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='contract',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Viktor Kotseruba',
+      author_email='barbuzaster@gmail.com',
+      url='https://github.com/barbuza/contract/',
+)
+
